6cea796da1c459b5ad2018823245e26857f1d8ad
freepina <freepina@gmail.com>
2015-10-13 21:58:55 -0400

    bunch of stuff

--- create.py
+++ create.py
@@ -4,10 +4,17 @@ conn = sqlite3.connect("data.db")
 
 c = conn.cursor()
 
+q = "drop table users"
+c.execute(q)
+q = "drop table posts"
+c.execute(q)
+q = "drop table comments"
+c.execute(q)
+
 q = "create table users(name text, password text, id integer)"
 c.execute(q)
 
-q = "create table posts(content text, pid integer, uid integer)"
+q = "create table posts(title text, content text, pid integer, uid integer)"
 c.execute(q)
 
 q = "create table comments(content text, cid integer, pid integer, uid integer)"
--- utils.py
+++ utils.py
@@ -1,25 +1,49 @@
 import sqlite3,hashlib
 
-def writePost(txt, idp, idu):
+def writePost(title,txt,idu):
+    conn = sqlite.connect('data.db')
+    cur = conn.cursor()
+    q = 'SELECT max(posts.id) FROM posts'
+    idp = cur.execute(q).fetchone()[0]
+    writePost(title, txt, idp, idu)
+    return idp
+
+def writePost(title, txt, idp, idu):
     conn = sqlite3.connect('data.db')
     cur = conn.cursor()
-    q = "INSERT INTO posts(content,pid,uid) VALUES(?,?,?)"
-    conn.execute(q,(txt,idp,idu))
+    q = "INSERT INTO posts(title,content,pid,uid) VALUES(?,?,?,?)"
+    conn.execute(q,(title,txt,idp,idu))
     conn.commit()
 
-#writePost("im sandy",2,1)
-#writePost("call me white fang",3,1)
-#writePost("im also white bread",4,1)
-#writePost("im the leader fear me",5,4)
+#writePost("title1","hello",1,1)
+#writePost("title2","im sandy",2,1)
+#writePost("title3","call me white fang",3,1)
+#writePost("title4","im also white bread",4,1)
+#writePost("title5","im the leader fear me",5,4)
+
+def getPost(idp):
+    conn = sqlite3.connect('data.db')
+    cur = conn.cursor()
+    q = "SELECT * FROM posts WHERE posts.pid = %d"
+    result = cur.execute(q%idp).fetchone()
+    return result
 
 def getPosts(idu):
     conn = sqlite3.connect('data.db')
     cur = conn.cursor()
-    q = "SELECT * FROM posts WHERE posts.uid = %s"
+    q = "SELECT * FROM posts WHERE posts.uid = %d"
     result = conn.execute(q%idu)
     for r in result:
         print r
     conn.commit()
+    return result
+
+def getComments(idp):
+    conn = sqlite3.connect('data.db')
+    cur = conn.cursor()
+    q = "SELECT * from comments WHERE comments.pid = %d"
+    result = conn.execute(q%idp).fetchall()
+    return result
 
 def encrypt(word):
     hashp = hashlib.md5()
@@ -36,6 +60,14 @@ def authenticate(username,password):
             return True
     return False
 
+def getUserId(name):
+    conn = sqlite3.connect('data.db')
+    cur = conn.cursor()
+    q = 'SELECT users.id FROM users WHERE users.name = %s'
+    result = conn.execute(q%name).fetchone()
+    print result
+    return result[0]
+
 def addUser(username,password):
     conn = sqlite3.connect('data.db')
     cur = conn.cursor()
@@ -44,15 +76,16 @@ def addUser(username,password):
     print len(result)
     if len(result) == 0:
         q = 'SELECT max(users.id) FROM users'
-        uid = conn.execute(q)
+        uid = cur.execute(q).fetchone()[0]
         q = 'INSERT INTO users VALUES (?, ?, ?)'
         conn.execute(q, (username, encrypt(password), uid+1))
         return True
     return False
-    
-getPosts(1)
-getPosts(2)
-getPosts(3)
-getPosts(4)
+
+print "hello"
+print getPost(1)
+#print getPost(2)
+#print getPost(3)
+#print getPost(4)
 
 
