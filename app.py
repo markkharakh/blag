214293bc61e88563d4236eef81abc8f6d7922713
Sandy Fang <sfang2016@gmail.com>
2015-10-13 20:27:20 -0400

    python file, in-progress

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, session
+from flask import redirect, url_for
+import sqlite3;
+import utils
+
+app = Flask(__name__)
+
+@app.route("/login")
+def login():
+	return render_template("login.html");
+
+@app.route("/home")
+def home():
+	conn = sqlite3.connect('data.db')
+	cur = conn.cursor()
+	q = "SELECT * FROM posts"
+	cur.execute(q)
+	all_rows = cur.fetchall()
+	return render_template("home.html", all_rows=all_rows)
+
+@app.route("/")
+def index():
+    return "hello"
+
+
+if __name__ == "__main__":
+   app.run(debug=True)
